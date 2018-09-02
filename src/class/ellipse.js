165b3e4f566f6a50d6a1b118c64c53afae16c6df
alec-petros <a.petros@verizon.net>
2018-09-02 17:05:47 -0400

    collision added but broken, separated update and draw, added gravity

--- src/class/ellipse.js
+++ src/class/ellipse.js
@@ -1,36 +1,89 @@
+var spaceVerb = new p5.Reverb();
+var circleArr = [];
+var index = 0;
+
 class Ellipse {
-  constructor(initX, initY, vector) {
+  constructor(initX, initY, velocity) {
+    this.index = index++;
+
     this.loc = createVector(initX, initY);
     this.accel = createVector(0, 0.005)
-    this.vector = vector
+    this.velocity = velocity
     this.scale = Math.random() * 100;
-    this.color = color(map(this.vector.x, -2.5, 2.5, 0, 255), map(this.scale, 0, 100, 0, 255), map(this.vector.y, -2.5, 2.5, 0, 255))
+    this.mass = map(this.scale, 0, 100, 1, 10)
+
+    //color stuff
+    this.colorScale = 1;
+    this.color = {
+      r: map(this.velocity.x, -2.5, 2.5, 0, 255),
+      g: map(this.scale, 0, 100, 0, 255),
+      b: map(this.velocity.y, -2.5, 2.5, 0, 255)
+    }
+
+    // osc stuff
     this.osc = new p5.Oscillator();
-    this.osc.setType('sine');
-    this.osc.freq(map(this.scale, 100, 0, 150, 1000));
-    this.osc.amp(0);
+    this.filter = new p5.LowPass()
+    this.filterFreq = 0;
+    this.filter.freq(this.filterFreq)
+    this.filter.res(2);
+    this.osc.disconnect();
+    this.osc.connect(this.filter);
+    this.osc.setType('triangle');
+    this.oscFreq = map(this.scale, 100, 0, 150, 1000)
+    this.osc.freq(this.oscFreq);
+    this.osc.amp(map(this.scale, 0, 100, 0.05, 0.5));
+    spaceVerb.process(this.filter, 5, 2)
     this.osc.start();
   }
 
+  applyForce(force) {
+    var f = p5.Vector.div(force, this.mass)
+    this.accel.add(f)
+  }
+
+  checkCollision() {
+    circleArr.forEach((circ) => {
+      if (this.index !== circ.index) {
+        if( dist( this.loc.x, this.loc.y, circ.loc.x, circ.loc.y) < (this.scale / 2 + circ.scale / 2 + 10) ) {
+          this.collide(false, false);
+          this.applyForce(circ.velocity)
+          circ.applyForce(this.velocity);
+          circ.collide(false, false);
+        }
+      }
+    })
+  }
+
   beep() {
-    this.osc.amp(map(this.scale, 0, 100, 0.2, 0.5), 0.05)
-    this.osc.amp(0, 3);
+    this.filterFreq = this.oscFreq;
+    this.colorScale = 1;
+  }
+
+  collide(x, y) {
+    this.beep()
+  }
+
+  checkWall() {
+    if (this.loc.x - this.scale / 2 < -this.velocity.x - canvas.width / 4 || this.loc.x + this.scale / 2 > canvas.width / 4 - this.velocity.x) {
+      this.collide(false, false)
+      this.velocity.x *= -1
+    }
+    if (this.loc.y - this.scale / 2 < -this.velocity.y - canvas.height / 4 || this.loc.y + this.scale / 2 > canvas.height / 4 - this.velocity.y) {
+      this.collide(false, true)
+      this.velocity.y *= -1
+    }
+  }
+
+  update() {
+    this.filter.freq(max(0, this.filterFreq -= map(this.scale, 100, 0, 0.5, 15)))
+    this.colorScale -= 0.002;
+    this.velocity.add(this.accel)
+    this.loc.add(this.velocity)
+    this.accel.mult(0)
   }
 
   draw() {
-    this.vector.x += this.accel.x;
-    this.vector.y += this.accel.y
-    if (this.loc.x < -this.vector.x - canvas.width / 4 || this.loc.x > canvas.width / 4 - this.vector.x) {
-      this.vector.x *= -1
-      this.beep()
-    }
-    if (this.loc.y < -this.vector.y - canvas.height / 4 || this.loc.y > canvas.height / 4 - this.vector.y) {
-      this.vector.y *= -1
-      this.beep()
-    }
-    this.loc.add(this.vector)
-
-    fill(this.color)
+    fill(color(this.color.r * this.colorScale, this.color.g * this.colorScale, this.color.b * this.colorScale));
     noStroke();
     ellipse(this.loc.x, this.loc.y, this.scale, this.scale)
   }
--- src/sketch.js
+++ src/sketch.js
@@ -4,20 +4,27 @@ function randomInt(scale) {
 
 var initV;
 var boy;
-var circleArr = [];
+var spaceVerb
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   initV = createVector(randomInt(1), randomInt(1))
   boy = new Ellipse(50, 50, initV)
-  for (let i = 0; i < 5; i++) {
-    circleArr.push(new Ellipse(randomInt(100), randomInt(100), createVector(randomInt(5), randomInt(5))))
+
+  for (let i = 0; i < 10; i++) {
+    circleArr.push(new Ellipse(randomInt(window.innerWidth - 100), randomInt(window.innerHeight - 100), createVector(randomInt(5), randomInt(5))))
   }
+  background(0);
 }
 
 function draw() {
-  background(255);
-  circleArr.forEach((circ) => {
-    circ.draw();
-  })
+  background(0)
+
+  for (let i = 0; i < circleArr.length; i++) {
+    let gravity = createVector(0, 0.1 * circleArr[i].mass);
+    circleArr[i].applyForce(gravity);
+    circleArr[i].update();
+    circleArr[i].draw();
+    circleArr[i].checkWall()
+  }
 }
